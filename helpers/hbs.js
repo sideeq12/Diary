753c76ca0969c9e047d4983af4a10f1b3ecacf85
sideeq12 <abdwaheed2018@gmail.com>
2021-01-04 01:34:49 -0800

    checking

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -1,6 +1,6 @@
 module.exports = {
     truncate : function(str, len){
-        if(str.length > len && str > 0){
+        if(str.length > len && str.length > 0){
             let new_str = str + " "
             new_str = str.substr(0, len)
             new_str = str.substr(0, new_str.lastIndexOf(""))
